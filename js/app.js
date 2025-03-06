7dade0c67d15af8c9312503e3b09cd3c2cf06012
Michael-Volpe <226mvolpe@frhsd.com>
2025-03-06 13:18:54 -0500

    second commit - js select elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-// CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById("date");
+const list  = document.getElementById("list");
+const input = document.getElementById("input");
