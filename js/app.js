094fbf215e4e22a0188ea57901bfa017ca2e54aa
Michael-Volpe <226mvolpe@frhsd.com>
2025-03-06 13:44:11 -0500

    fifth commit - 13:36 into vid

--- js/app.js
+++ js/app.js
@@ -20,8 +20,18 @@ document.addEventListener("keyup", function(event){
     if(event.keyCode == 13){
         const toDo = input.value;
         if(toDo){
-            addToDo(toDo);
+            addToDo(toDo,id, false, false);
+            LIST.push(
+                {
+                    name: toDo,
+                    id: id,
+                    done: false,
+                    trash: false
+                }
+            );
+            input.value = "";
+            id++;
         }
-        input.value = "";
     }
 });
+
