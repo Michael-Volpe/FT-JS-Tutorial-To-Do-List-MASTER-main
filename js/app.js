80f7bfc0d02e08cbb5ffa46e8c6cbe9952fa600b
Michael-Volpe <226mvolpe@frhsd.com>
2025-03-07 13:07:19 -0500

    seventh commit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
+//Select the elements
 const clear = document.querySelector(".clear");
 const dataElement = document.getElementById("date");
 const list  = document.getElementById("list");
-
-function addToDo(toDo) {
-    const text = `<li class="item">
-        <i class="co fa fa-circle thin" job="complete"></i>
-        <p class="text"> ${toDo} </p>
-        <i class="de fa fa-trash-o" job="delete"></i>
-    </li>`
-}
+const input = document.getElementById("input");
 
 //Classes names
 const CHECK = "fa-check-circle";
@@ -20,29 +14,20 @@ const options = {weekday: "long", month:"short", day:"numeric"};
 const today = new Date();
 dateElements.innerHTML = today.toLocaleDateString("en-US", options);
 
+//add to do function
+
+function addToDo(toDo) {
+
+    const item = `<li class="item">
+    <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+    <p class="text">${toDo}</p>
+    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+    </li>
+`;
 
 const position = "beforeend";
 
-list.insertAdjacentHTML(position,text);
+list.insertAdjacentHTML(position, item);
+}
 
-const input = document.getElementById("input");
-
-document.addEventListener("keyup", function(event){
-    if(event.keyCode == 13){
-        const toDo = input.value;
-        if(toDo){
-            addToDo(toDo,id, false, false);
-            LIST.push(
-                {
-                    name: toDo,
-                    id: id,
-                    done: false,
-                    trash: false
-                }
-            );
-            input.value = "";
-            id++;
-        }
-    }
-});
-
+addToDo("drink coffee");
